9b959eb915d7107468d4cf071d8cc7e4158ee39b
AZAwhite13 <youremail@example.com>
2025-11-05 04:01:49 +0500

    Fix: update app

--- i/vite.config.ts
+++ i/vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+// i/vite.config.ts
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000
+  }
 })
